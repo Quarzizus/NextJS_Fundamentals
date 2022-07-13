052c284e682cd5becaa7925d985bf2219f8c0326
Miguel Vásquez <angelzizu06@gmail.com>
2022-07-13 14:41:37 -0500

    :sparkles: to add components of next.js
    link and proxy

--- components/Avocado/index.tsx
+++ components/Avocado/index.tsx
@@ -1,19 +1,21 @@
+import React from "react";
 import Image from "next/image";
 import styles from "./styles.module.css";
 import { TProduct } from "../../database/types";
+import Link from "next/link";
 
 const Avocado = ({ name, id, image, price, sku }: TProduct) => {
   return (
-    <li className={styles.Avocado}>
-      <a href={`/product/${id}`}>
-        <Image src={image} width="195px" height="195px" />
-      </a>
-      <div>
-        <h3>{name}</h3>
-        <p>{price}</p>
-        <p>{sku}</p>
-      </div>
-    </li>
+    <Link href={`/product/${id}`}>
+      <li className={styles.Avocado}>
+        <Image src={image} width="200px" height="210px" />
+        <div>
+          <h3>{name}</h3>
+          <p>{price}</p>
+          <p>{sku}</p>
+        </div>
+      </li>
+    </Link>
   );
 };
 
